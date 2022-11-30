f08c4b73c26087d11a83f2cd438ab2f189c2ac16
ndam jonah <ndamjoh@gmail.com>
2022-11-30 11:44:45 +0100

    fix bog on api

--- pages/api/resend-token.js
+++ pages/api/resend-token.js
@@ -1,7 +1,7 @@
 import { verifyEmailTmp } from "../../emailTmps/verifyEmailTmp";
 import dbConnect from "../../lib/dbConnect";
 import User from "../../models/User";
-import VerifyUser from "../../models/VerifyUser";
+import AccessToken from "../../models/AccessToken";
 
 export default async function handler(req, res) {
   await dbConnect();
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
           .json({ success: false, message: "email not found" });
 
       // get existing token from verify table
-      const token = VerifyUser.findById(user._id);
+      const token = await  AccessToken.findById(user.email);
 
       // send email
       verifyEmailTmp(user, token.token);
